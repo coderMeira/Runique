ff3aa302dfd820fdd8d2ba320d84b781fb5f4b10
Frederico Coder Meira <fredericomeiraa@gmail.com>
2026-02-20 20:40:28 +0000

    feat: add project configuration files and enhance Result mapping functionality

--- core/domain/src/main/java/com/example/core/domain/util/Error.kt
+++ core/domain/src/main/java/com/example/core/domain/util/Error.kt
@@ -1,4 +1,3 @@
 package com.example.core.domain.util
 
-interface Error {
-}
+interface Error
--- core/domain/src/main/java/com/example/core/domain/util/Result.kt
+++ core/domain/src/main/java/com/example/core/domain/util/Result.kt
@@ -5,7 +5,15 @@ sealed interface Result<out D, out Error> {
     data class Error<out E : com.example.core.domain.util.Error>(val error: E) : Result<Nothing, E>
 }
 
-inline fun <T, E : Error> Result<T, E>.map(transform: (T) -> T): Result<T, E> = when (this) {
-    is Result.Success -> Result.Success(transform(data))
-    is Result.Error -> this
+inline fun <T, E : Error, R> Result<T, E>.map(transform: (T) -> R): Result<R, E> {
+    return when (this) {
+        is Result.Success -> Result.Success(transform(data))
+        is Result.Error -> this
+    }
 }
+
+fun <T, E : Error> Result<T, E>.asEmptyDataResult(): EmptyDataResult<E> {
+    return map { }
+}
+
+typealias EmptyDataResult<E> = Result<Unit, E>
